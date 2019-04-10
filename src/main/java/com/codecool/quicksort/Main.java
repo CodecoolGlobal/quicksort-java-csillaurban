5d2e0a6a24f0cbad14f01c0938de76b7f31a57bc
Csilla Urban <csillaurban@gmail.com>
2019-04-11 00:24:41 +0200

    quicksort

--- src/main/java/com/codecool/quicksort/Main.java
+++ src/main/java/com/codecool/quicksort/Main.java
@@ -1,9 +1,18 @@
 package com.codecool.quicksort;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         // Use the tests to check your code!
+        QuickSort quicksort = new QuickSort();
+
+        Integer numbers[] = new Integer[] {32, 76, 2, 8, 7, 19, 56, 10};
+        List<Integer> toSort = Arrays.asList(numbers);
+
+        quicksort.sort(toSort);
     }
 
 }
--- src/main/java/com/codecool/quicksort/QuickSort.java
+++ src/main/java/com/codecool/quicksort/QuickSort.java
@@ -1,6 +1,9 @@
 package com.codecool.quicksort;
 
+import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.swap;
 
 public class QuickSort {
 
@@ -9,6 +12,62 @@ public class QuickSort {
      * @param toSort the List to sort. Throws an error if its null
      */
     public void sort(List<Integer> toSort) {
+            if(toSort == null) {
+                throw new IllegalArgumentException();
+            }
 
+            Collections.shuffle(toSort);
+            sortingSides(toSort, 0, toSort.size() - 1);
+    }
+
+    private void sortingSides(List<Integer> toSort, int lowestIndex, int highestIndex) {
+            if(highestIndex <=lowestIndex) {
+                return;
+            }
+            //get the position of sorted pivot, which divides the sides
+            int divider = partition(toSort, lowestIndex, highestIndex);
+            //sort the left side
+            sortingSides(toSort, lowestIndex, divider-1);
+            //sort the right side
+            sortingSides(toSort, divider+1, highestIndex);
+    }
+
+    private int partition(List<Integer> toSort, int lowestIndex, int highestIndex) {
+        int i = lowestIndex;
+        int j = highestIndex;
+
+        int pivot = toSort.get(lowestIndex);
+
+        while(true) {
+            //starting from the left, while item on index 'i' smaller than the pivot, step to the right
+            while(toSort.get(i) <= pivot) {
+                i++;
+                //if the steps reach the end of the list, break
+                if(i == highestIndex) {
+                    break;
+                }
+            //starting from the right, while item on index 'i' larger than the pivot, step to the left
+            } while(toSort.get(j) >= pivot) {
+                j--;
+                //if the steps reach the start of the list, break
+                if(j == lowestIndex) {
+                    break;
+                }
+            }
+
+            //if the steps from left and right crossed, break
+            if(i >= j) {
+                break;
+            }
+
+            //if item from the left is bigger than the pivot AND the item from the right is smaller than the pivot,
+            //i and j not crossed, swap them in the list
+            swap(toSort, i, j);
+        }
+
+        //if i and j crossed, swap the pivot with the biggest smaller element than the pivot, the right side of the left block
+
+        swap(toSort, lowestIndex, j);
+        return j;
     }
 }
